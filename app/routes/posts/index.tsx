48ec29b5bf4179fa2500e115fc17628fde864588
taiki-nishi <taiki-nishi@cybozu.co.jp>
2021-11-25 13:28:27 +0900

    init

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,23 @@
+import { useLoaderData, Link } from "remix";
+import { Post, getPosts } from '~/post'
+
+export const loader = async () => {
+	return await getPosts();
+};
+
+export default function Posts() {
+  const posts = useLoaderData<Post[]>();
+  console.log(posts)
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
